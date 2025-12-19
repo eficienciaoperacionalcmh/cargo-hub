18abee3646bbcf2e8e532aa7baaefd3d8da7b77c
eficienciaoperacionalcmh <142448936+eficienciaoperacionalcmh@users.noreply.github.com>
2025-12-19 11:23:20 -0500

    Update page.jsx

--- app/(dashboards)/dashboard/infoCarga/page.jsx
+++ app/(dashboards)/dashboard/infoCarga/page.jsx
@@ -1,1 +1,20 @@
+'use client'
 
+import { Fragment, useState } from 'react'
+import ViewRefresh from '@/app/(dashboards)/refreshView'
+import ViewTableau from '@/app/(dashboards)/tableauView'
+
+export default function Home() {
+    // const [autoUpdate, setAutoUpdate] = useState(true);
+    const url_tableau = "https://i2trekviz.somoscopa.com/t/CopaVizHub/views/Infocarga-CM/Infocapacidaddecarga"
+    // const refresh_time_ms = 30000
+    return (
+        <Fragment>
+            <ViewTableau
+                autoUpdate={autoUpdate}
+                url_tableau={url_tableau}
+                refresh_time_ms={refresh_time_ms}
+            />
+        </Fragment>
+    )
+}
